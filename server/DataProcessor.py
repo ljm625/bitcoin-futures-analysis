8959315a1f0606bc46caeea75d05066bd1db05c8
Jiaming Li <ljm625@gmail.com>
2018-09-27 16:07:33 +0800

    added feature for time based summary on Rest APIs

--- server/DataProcessor.py
+++ server/DataProcessor.py
@@ -1,3 +1,5 @@
+import datetime
+
 import yaml
 
 from MongoController import MongoController
@@ -17,7 +19,7 @@ class DataProcessor(object):
         self.db_handler = MongoController(self.config.get("mongo_uri"),self.config.get("mongo_username"),self.config.get("mongo_password"))
 
 
-    async def get_order_data(self,coin,period,interval,start_date=None):
+    async def get_order_data(self,coin,period,interval,start_date=None,minimum=None):
         """
 
         :param coin: coin type, [bitcoin, ethereum]
@@ -41,6 +43,9 @@ class DataProcessor(object):
         y2 = [0 for _ in range(0,(max_price-min_price)//interval+1)]
 
         for data in datas:
+            if minimum:
+                if abs(data['amount'])<minimum:
+                    continue
             y[int((data["price"] - min_price) // interval)] += abs(data["amount"])
             y2[int((data["price"] - min_price) // interval)] += data["amount"]
         for i in range(0, len(x)):
@@ -48,7 +53,7 @@ class DataProcessor(object):
                 y[i] = -y[i]
         return [x,y]
 
-    async def get_order_data_time(self,coin,period,interval,start_date=None):
+    async def get_order_time_series_data(self,coin,period,interval,start_date=None,minimum=None):
         """
 
         :param coin: coin type, [bitcoin, ethereum]
@@ -57,29 +62,32 @@ class DataProcessor(object):
         :param start_date: the Unix timestamp * 1000
         :return: [x,y] x is price list, y is amount list.
         """
-        def query_string_builder():
-            time_now = int(time.time() * 1000)
-            if start_date:
-                return {"time": {"$lt": start_date+(3600*period)*1000,"$gt":start_date}}
-            else:
-                return {"time": {"$lt": time_now,"$gt":time_now-(3600*period)*1000}}
 
+        def query_string_builder():
+            return {"time": {"$lt": end_time,"$gt":start_time}}
+        if not start_date:
+            end_time = int(time.time()*1000)
+            start_time = end_time - period*3600*1000
+        else:
+            start_time = start_date
+            end_time = start_time + period*3600*1000
         datas = await self.db_handler.get(coin,"orders",query_string_builder())
-        max_price = int((max(datas, key=lambda x: x["price"])["price"]//interval+1)*interval)
-        min_price = int((min(datas, key=lambda x: x["price"])["price"]//interval)*interval)
-        x = [min_price+i*interval for i in range(0,(max_price-min_price)//interval+1)]
-        y = [0 for _ in range(0,(max_price-min_price)//interval+1)]
-        y2 = [0 for _ in range(0,(max_price-min_price)//interval+1)]
+        interval_timestamp = interval*3600*1000
+
+        x = [start_time+i*interval_timestamp for i in range(0,(end_time-start_time)//interval_timestamp)]
+        y = [0 for _ in range(0,len(x))]
+        y2 = [0 for _ in range(0,len(x))]
 
         for data in datas:
-            y[int((data["price"] - min_price) // interval)] += abs(data["amount"])
-            y2[int((data["price"] - min_price) // interval)] += data["amount"]
-        for i in range(0, len(x)):
-            if y2[i] < 0:
-                y[i] = -y[i]
-        return [x,y]
+            if minimum:
+                if abs(data['amount'])<minimum:
+                    continue
 
-
+            if data["amount"]>=0:
+                y[int((data["time"] - start_time) // interval_timestamp)] += data["amount"]
+            else:
+                y2[int((data["time"] - start_time) // interval_timestamp)] += data["amount"]
+        return [x,y,y2]
 
 
 
--- server/RestServer.py
+++ server/RestServer.py
@@ -26,23 +26,66 @@ class OrderDataHandler(tornado.web.RequestHandler):
             assert type(data.get("period"))==int
             assert type(data.get("interval")) in (int,float)
             assert type(data.get("start_date")) in (int,float,type(None))
+            assert type(data.get("min")) in (int,float,type(None))
+
         except Exception as e:
             self.write(json.dumps({"error":"params are not valid"}))
             return
         try:
             if coin == "eth":
-                result = await self._dp.get_order_data("ethereum",data.get("period"),data.get("interval"),data.get("start_date"))
+                result = await self._dp.get_order_data("ethereum",data.get("period"),data.get("interval"),data.get("start_date"),minimum=data.get("min"))
                 self.write(json.dumps({"price":result[0],"amount":result[1]}))
             elif coin == "btc":
-                result = await self._dp.get_order_data("bitcoin",data.get("period"),data.get("interval"),data.get("start_date"))
+                result = await self._dp.get_order_data("bitcoin",data.get("period"),data.get("interval"),data.get("start_date"),minimum=data.get("min"))
                 self.write(json.dumps({"price":result[0],"amount":result[1]}))
         except Exception as e:
             self.write(json.dumps({"error":str(e)}))
 
 
+class TimeOrderDataHandler(tornado.web.RequestHandler):
+
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self._dp=DataProcessor()
+
+
+    async def get(self,coin):
+        if coin=="eth":
+            # Check ETH orders
+            pass
+        elif coin == "btc":
+            pass
+        self.write("{}")
+
+    async def post(self,coin):
+        try:
+            data = json.loads(self.request.body)
+            assert type(data.get("period"))==int
+            assert type(data.get("interval")) in (int,float)
+            assert type(data.get("start_date")) in (int,float,type(None))
+            assert type(data.get("min")) in (int,float,type(None))
+
+        except Exception as e:
+            self.write(json.dumps({"error":"params are not valid"}))
+            return
+        try:
+            if coin == "eth":
+                result = await self._dp.get_order_time_series_data("ethereum",data.get("period"),data.get("interval"),data.get("start_date"),minimum=data.get("min"))
+                self.write(json.dumps({"time":result[0],"buy_amount":result[1],"sell_amount":result[2]}))
+            elif coin == "btc":
+                result = await self._dp.get_order_time_series_data("bitcoin",data.get("period"),data.get("interval"),data.get("start_date"),minimum=data.get("min"))
+                self.write(json.dumps({"time":result[0],"buy_amount":result[1],"sell_amount":result[2]}))
+        except Exception as e:
+            self.write(json.dumps({"error":str(e)}))
+
+
+
+
+
 if __name__ == "__main__":
     application = tornado.web.Application([
         (r"/orders/(.[a-zA-Z]+)", OrderDataHandler),
+        (r"/time_orders/(.[a-zA-Z]+)", TimeOrderDataHandler),
 
     ])
     application.listen(9888)
